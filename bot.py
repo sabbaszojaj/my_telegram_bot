4f15362db57f4c323f25269d3bd930b602e40a26
sabbaszojaj <sabbaszojaji@gmail.com>
2025-07-20 09:21:16 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! من زنده‌ام 😊")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
